e20ebfcb27903b0e7a438b3a0fa98053c86d500b
janithBimsaraUlukkulama <jbimsaraulukkulama@outlook.com>
2022-01-14 00:56:49 +0530

    added chat rooms

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "livechat": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "livechat1ea87ec5": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateMessage = /* GraphQL */ `
+  subscription OnCreateMessage {
+    onCreateMessage {
+      id
+      owner
+      message
+      chatRoomId
+      readStatus
+      messageType
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateMessage = /* GraphQL */ `
+  subscription OnUpdateMessage {
+    onUpdateMessage {
+      id
+      owner
+      message
+      chatRoomId
+      readStatus
+      messageType
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteMessage = /* GraphQL */ `
+  subscription OnDeleteMessage {
+    onDeleteMessage {
+      id
+      owner
+      message
+      chatRoomId
+      readStatus
+      messageType
+      createdAt
+      updatedAt
+    }
+  }
+`;
